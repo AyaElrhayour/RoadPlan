392f29cc72c8b250944e72cfc9f0a689b1d713cc
Aya <ayaelrhayour123@gmail.com>
2025-01-08 16:15:24 +0100

    Created tags entity

---
+++ src/main/java/com/youcode/roadplan/Models/Entities/Tags.java
@@ -0,0 +1,23 @@
+package com.youcode.roadplan.Models.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tags {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    private String name;
+}
