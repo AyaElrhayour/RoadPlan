63be75b5cab6294fb3fa243bd83c472285adee37
Aya <ayaelrhayour123@gmail.com>
2025-01-08 15:19:56 +0100

    Project creation

---
+++ src/main/java/com/youcode/roadplan/RoadPlanApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.roadplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youcode/roadplan/RoadPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.roadplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
