8c35dfc27e3e027399716cb9198905b0abb3b2eb
Aya <ayaelrhayour123@gmail.com>
2025-01-09 11:39:26 +0100

    Created Transport DTO

---
+++ src/main/java/com/youcode/roadplan/Models/DTOs/TransportDto.java
@@ -0,0 +1,20 @@
+package com.youcode.roadplan.Models.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransportDto {
+
+    private UUID id;
+    private String city;
+    private List<UUID> hotels;
+    private List<UUID> activities;
+    private List<UUID> transportOptions;
+}
