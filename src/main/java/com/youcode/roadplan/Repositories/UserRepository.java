4c494093320fa3db239921c26a92a17b180adf15
Aya <ayaelrhayour123@gmail.com>
2025-01-13 10:26:05 +0100

    Created User Repository

---
+++ src/main/java/com/youcode/roadplan/Repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.youcode.roadplan.Repositories;
+
+import com.youcode.roadplan.Models.Entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
